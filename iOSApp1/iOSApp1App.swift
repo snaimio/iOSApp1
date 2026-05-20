c455a491e0f20a9311a327d23bbaf6fa6a6e72cb
Sheikh Naim <naimbgd@gmail.com>
2026-05-20 12:18:08 -0400

    Initial Commit

---
+++ iOSApp1/iOSApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp1App.swift
+//  iOSApp1
+//
+//  Created by Sheikh Naim on 2026-05-20.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
